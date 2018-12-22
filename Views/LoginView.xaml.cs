d49bb5c30ec1834317c84539a8f04e9196cb2b94
Sannan <bscs15009@itu.edu.pk>
2018-12-22 11:01:49 +0500

    commi

---
+++ Views/LoginView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataBindingDemo
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (var context = new mainEntities())
+            {
+                var query = from p in context.Users
+                            where p.UserName == username.Text.ToString()
+                            && p.Password == password.Text.ToString()
+                            select p;
+
+                if (query.Any())
+                {
+                    DialogResult=true;
+                }
+            }
+        }
+    }
+}
